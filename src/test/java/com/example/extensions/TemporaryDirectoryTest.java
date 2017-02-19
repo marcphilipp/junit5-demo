e4f6a871f079c64a4e3fca170e73d9d3760eee1d
Marc Philipp <mail@marcphilipp.de>
2017-02-19 19:35:51 +0100

    Base for extension examples

---
+++ src/test/java/com/example/extensions/TemporaryDirectoryTest.java
@@ -0,0 +1,24 @@
+package com.example.extensions;
+
+import static java.util.Arrays.asList;
+import static org.junit.jupiter.api.Assertions.assertIterableEquals;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+class TemporaryDirectoryTest {
+
+    @Test
+    void writeAndReadFile(Path tempDir) throws Exception {
+        Path testFile = tempDir.resolve("test.txt");
+
+        Files.write(testFile, asList("foo", "bar"));
+
+        List<String> actualLines = Files.readAllLines(testFile);
+        assertIterableEquals(asList("foo", "bar"), actualLines);
+    }
+
+}
