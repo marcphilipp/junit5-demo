a7750e6b273920a4ed77a3e84fc9cdafd80a483b
Marc Philipp <mail@marcphilipp.de>
2017-04-04 22:01:44 +0200

    After extensions demo

--- src/test/java/com/example/extensions/DisabledOnConference.java
+++ src/test/java/com/example/extensions/DisabledOnConference.java
@@ -5,7 +5,10 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import org.junit.jupiter.api.extension.ExtendWith;
+
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
+@ExtendWith(DisabledOnConferenceExtension.class)
 public @interface DisabledOnConference {
 }
---
+++ src/test/java/com/example/extensions/DisabledOnConferenceExtension.java
@@ -0,0 +1,17 @@
+package com.example.extensions;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.extension.ConditionEvaluationResult;
+import org.junit.jupiter.api.extension.TestExecutionCondition;
+import org.junit.jupiter.api.extension.TestExtensionContext;
+
+public class DisabledOnConferenceExtension implements TestExecutionCondition {
+    @Override
+    public ConditionEvaluationResult evaluate(TestExtensionContext context) {
+        return LocalDate.now().getDayOfWeek() == DayOfWeek.TUESDAY
+                ? ConditionEvaluationResult.disabled("You're at a conference")
+                : ConditionEvaluationResult.enabled("Get some work done!");
+    }
+}
