123f4419b1ef8ce892b9776737c1ec13cd44baf5
Marc Philipp <mail@marcphilipp.de>
2017-06-04 17:42:43 +0300

    Basic extension examples

--- src/test/java/com/example/extensions/TemporaryDirectoryTest.java
+++ src/test/java/com/example/extensions/TemporaryDirectoryTest.java
@@ -1,17 +1,19 @@
 package com.example.extensions;
 
-import static java.util.Arrays.asList;
-import static org.junit.jupiter.api.Assertions.assertIterableEquals;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
 
-import org.junit.jupiter.api.Test;
+import static java.util.Arrays.asList;
+import static org.junit.jupiter.api.Assertions.assertIterableEquals;
 
 class TemporaryDirectoryTest {
 
     @Test
+    @ExtendWith(TemporaryDirectoryExtension.class)
     void writeAndReadFile(Path tempDir) throws Exception {
         Path testFile = tempDir.resolve("test.txt");
 
