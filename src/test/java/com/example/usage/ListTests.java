e45418305e27b717315006da5db1fa340c1e9483
Marc Philipp <mail@marcphilipp.de>
2018-04-25 15:10:41 +0200

    Basics Demo

--- src/test/java/com/example/usage/ListTests.java
+++ src/test/java/com/example/usage/ListTests.java
@@ -1,4 +1,46 @@
 package com.example.usage;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@DisplayName("A list")
+@TestInstance(Lifecycle.PER_CLASS)
 class ListTests {
+
+    private List<Object> list;
+
+    @BeforeEach
+    void setUp() {
+        list = new ArrayList<>();
+    }
+
+    @Test
+    @DisplayName("should contain previously added element")
+    @Tag("happy-path")
+    void addingAnElement() {
+        list.add("foo");
+
+        assertEquals("foo", list.get(0));
+        assertTrue(list.contains("foo"),
+                () -> "list should contain 'foo': " + list);
+    }
+
+    @Test
+    void removeMissingElement() {
+        IndexOutOfBoundsException exception = assertThrows(IndexOutOfBoundsException.class,
+                () -> list.remove(0));
+
+        assertTrue(exception.getMessage().contains("0"));
+    }
 }
