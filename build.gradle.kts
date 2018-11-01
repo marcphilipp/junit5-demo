b9c02651067d3b1c65f9ba0f269ca4f7fe0d4d83
Marc Philipp <mail@marcphilipp.de>
2018-11-01 16:42:58 +0100

    Upgrade to Jupiter 5.3.1 and convert build scripts to Kotlin DSL

---
+++ build.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+    java
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.3.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.3.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.3.1")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
