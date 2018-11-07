15daaee0b376810291228bfe1bd80f5c0baf5e2f
Marc Philipp <mail@marcphilipp.de>
2018-11-07 11:31:03 +0100

    Prepare junit-pioneer dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,6 +9,7 @@ repositories {
 dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.3.1")
     testImplementation("org.junit.jupiter:junit-jupiter-params:5.3.1")
+//    testImplementation("org.junit-pioneer:junit-pioneer:0.3.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.3.1")
 }
 
