813e7331dded4be48a2c4e8480f0d8dcd3e9ce96
Marc Philipp <mail@marcphilipp.de>
2018-11-07 18:52:02 +0100

    Enable parallel test execution
    # Conflicts:
    #	src/test/java/com/example/usage/MathTests.java

--- src/test/java/com/example/usage/SystemPropertiesTests.java
+++ src/test/java/com/example/usage/SystemPropertiesTests.java
@@ -3,9 +3,15 @@ package com.example.usage;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.parallel.ResourceAccessMode;
+import org.junit.jupiter.api.parallel.ResourceLock;
+import org.junit.jupiter.api.parallel.Resources;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.parallel.ResourceAccessMode.READ;
+import static org.junit.jupiter.api.parallel.ResourceAccessMode.READ_WRITE;
+import static org.junit.jupiter.api.parallel.Resources.SYSTEM_PROPERTIES;
 
 class SystemPropertiesTests {
 
@@ -16,12 +22,14 @@ class SystemPropertiesTests {
     }
 
     @Test
+    @ResourceLock(value = SYSTEM_PROPERTIES, mode = READ_WRITE)
     void writeAndRead() {
         System.setProperty("foo", "bar");
         assertEquals("bar", System.getProperty("foo"));
     }
 
     @Test
+    @ResourceLock(value = SYSTEM_PROPERTIES, mode = READ)
     void readOnly() {
         assertNull(System.getProperty("foo"));
     }
