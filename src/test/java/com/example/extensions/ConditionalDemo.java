59cb01c369c6fe3c9edffb075b02ad824b2f79f8
Marc Philipp <mail@marcphilipp.de>
2022-09-27 17:57:58 +0200

    It's Tuesday

--- src/test/java/com/example/extensions/ConditionalDemo.java
+++ src/test/java/com/example/extensions/ConditionalDemo.java
@@ -3,15 +3,14 @@ package com.example.extensions;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
-import static java.time.DayOfWeek.SATURDAY;
-import static java.time.DayOfWeek.SUNDAY;
+import static java.time.DayOfWeek.TUESDAY;
 import static org.junit.jupiter.api.Assertions.fail;
 
 @Disabled("not yet implemented")
 class ConditionalDemo {
 
     @Test
-    @DisabledOnWeekdays({SATURDAY, SUNDAY})
+    @DisabledOnWeekdays(TUESDAY)
     void conditionalTest() {
         fail("fix me");
     }
