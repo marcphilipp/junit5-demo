d71a548b4dbd89a35c73929bc0a583b258a4c801
Marc Philipp <mail@marcphilipp.de>
2022-09-27 17:57:41 +0200

    Use Java 17

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,12 @@ repositories {
     mavenCentral()
 }
 
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
+
 dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter:5.9.1")
 }
