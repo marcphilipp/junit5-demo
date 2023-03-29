c58591745fffe52a07c43077b793248d25848a17
Marc Philipp <mail@marcphilipp.de>
2023-03-29 08:01:30 +0200

    Add JFR support

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,8 +14,19 @@ java {
 
 dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
+    testRuntimeOnly("org.junit.platform:junit-platform-jfr") {
+        because("it adds support for JUnit's JFR events")
+    }
 }
 
 tasks.test {
     useJUnitPlatform()
+
+    // Enable JFR
+    jvmArgumentProviders += CommandLineArgumentProvider {
+        listOf(
+            "-XX:StartFlightRecording=filename=${reports.junitXml.outputLocation.asFile.get().absolutePath},dumponexit=true",
+            "-XX:FlightRecorderOptions=stackdepth=1024"
+        )
+    }
 }
