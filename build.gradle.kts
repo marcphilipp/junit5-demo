99e7da7c304a39ed0661ec6de83f2b490da011a3
Marc Philipp <mail@marcphilipp.de>
2023-03-29 08:02:08 +0200

    Enable generation of new XML reports

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,6 +17,9 @@ dependencies {
     testRuntimeOnly("org.junit.platform:junit-platform-jfr") {
         because("it adds support for JUnit's JFR events")
     }
+    testRuntimeOnly("org.junit.platform:junit-platform-reporting") {
+        because("it adds support for the new XML reporting format")
+    }
 }
 
 tasks.test {
@@ -27,6 +30,14 @@ tasks.test {
         listOf(
             "-XX:StartFlightRecording=filename=${reports.junitXml.outputLocation.asFile.get().absolutePath},dumponexit=true",
             "-XX:FlightRecorderOptions=stackdepth=1024"
+        )
+    }
+
+    // Enable new XML reporting format
+    jvmArgumentProviders += CommandLineArgumentProvider {
+        listOf(
+            "-Djunit.platform.reporting.open.xml.enabled=true",
+            "-Djunit.platform.reporting.output.dir=${reports.junitXml.outputLocation.asFile.get().absolutePath}"
         )
     }
 }
